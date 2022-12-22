bcc8f1f5909dfd3cb1f73a8cdf5d67433e2d9df4
hanmok <dmammmm@naver.com>
2022-12-23 00:20:16 +0900

    done bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const app = express();
 
@@ -24,7 +28,7 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 
 const User = new mongoose.model('User', userSchema);
@@ -42,23 +46,32 @@ app.get("/register", (req, res) => {
 })
  
 app.post("/register", function(req, res) { 
-	const newUser = new User({
-		email: req.body.username,
-		password: req.body.password
+
+	bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+		const newUser = new User({
+			email: req.body.username,
+			// password: req.body.password
+			// password: md5(req.body.password)
+			password: hash
+		});
+	
+		newUser.save(function(err){
+			if (err) {
+				console.log(err);
+			} else { 
+				res.render("secrets");
+			}
+		})
 	});
 
-	newUser.save(function(err){
-		if (err) {
-			console.log(err);
-		} else { 
-			res.render("secrets");
-		}
-	})
+	
 })
 
 app.post("/login", function(req, res) {
 	const username = req.body.username;
-	const password = req.body.password;
+
+	bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+		const password = md5(req.body.password);
 
 	User.findOne({email: username}, function(err, foundUser){
 		if (err) { 
@@ -71,6 +84,22 @@ app.post("/login", function(req, res) {
 			}
 		}
 	})
+	
+	})
+
+	// const password = md5(req.body.password);
+
+	// User.findOne({email: username}, function(err, foundUser){
+	// 	if (err) { 
+	// 		console.log(err);
+	// 	} else { 
+	// 		if (foundUser) { 
+	// 			if (foundUser.password === password) { 
+	// 				res.render("secrets")
+	// 			}
+	// 		}
+	// 	}
+	// })
 })
 
 app.listen(3000, function() { 
